39b70b2e2bd34441a7d225bf600e6ead35fcef1d
Georgios Ivanov <yorgo_ivanov@abv.bg>
2021-06-07 14:10:19 +0300

    Update home.js

--- Project_weather/static/js/home.js
+++ Project_weather/static/js/home.js
@@ -7,9 +7,8 @@ function showPosition(position) {
     console.log(position.coords);
     lat = position.coords.latitude;
     lon = position.coords.longtitude;
-  }
-
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=95bd16a7252d408dc3c216d4e6bfbae3`)
+  
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=95bd16a7252d408dc3c216d4e6bfbae3`)
             .then( (response) => {
               if (response.status >= 200 && response.status < 300) {
                   console.log("There is a problem!");
@@ -22,3 +21,5 @@ function showPosition(position) {
             .catch((err) => {
           console.log("the following error occured");
         });   
+  }
+
