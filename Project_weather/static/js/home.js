98e86020fff42ad76a87060884b21d0685eccf9c
Georgios Ivanov <yorgo_ivanov@abv.bg>
2021-06-09 16:08:44 +0300

    repost

--- Project_weather/static/js/home.js
+++ Project_weather/static/js/home.js
@@ -1,6 +1,7 @@
 let lat;
 let lon;
 
+
 window.onload = navigator.geolocation.getCurrentPosition(showPosition);
 
 function showPosition(position) {
@@ -17,15 +18,12 @@ function showPosition(position) {
       
               response.json().then( (data) => {
               console.log(data);
-              let today_icon_url = data.weather[0].icon;
-              let today_icon = document.getElementById('today_icon');
-              today_icon.src = `http://openweathermap.org/img/wn/${today_icon_url}.png`;
+              document.getElementById('today_icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
               document.getElementById('today_temp').innerHTML = (data.main.temp - 273.15).toFixed(0) + "°";
               document.getElementById('today_humidity').innerHTML = data.main.humidity+ "%";
               document.getElementById('today_rain').innerHTML = data.rain;
               document.getElementById('today_wspeed').innerHTML = data.wind.speed + "meter/sec";
-              let today_clouds = document.getElementById('today_clouds');
-              today_clouds.innerHTML = data.clouds.all+ "%";
+              document.getElementById('today_clouds').innerHTML = data.clouds.all+ "%";
               })
           })       
             .catch((err) => {
@@ -34,7 +32,7 @@ function showPosition(position) {
         
         //3days fetch
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily&appid=95bd16a7252d408dc3c216d4e6bfbae3`)
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=95bd16a7252d408dc3c216d4e6bfbae3`)
         .then( (response) => {
           if (response.status < 200 && response.status > 300) {
               console.log("There is a problem!");
@@ -42,6 +40,24 @@ function showPosition(position) {
   
           response.json().then( (data) => {
           console.log(data);
+             for(let i=0 ;i < 3 ;i++)
+             {
+              document.getElementById('3days_icon'+i).src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`;
+              document.getElementById('3days_temp'+i).innerHTML =(data.daily[i].temp.day - 273.15).toFixed(0) + "°";
+              document.getElementById('3days_humidity'+i).innerHTML = data.daily[i].humidity+ "%";
+              document.getElementById('3days_rain'+i).innerHTML = data.daily[i].rain;
+              document.getElementById('3days_windSpeed'+i).innerHTML = data.daily[i].wind_speed + "meter/sec";
+              document.getElementById('3days_clouds'+i).innerHTML = data.daily[i].clouds+ "%";
+             }
+             for(let i=0 ;i < 7 ;i++)
+             {
+              document.getElementById('7days_icon'+i).src = `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`;
+              document.getElementById('7days_temp'+i).innerHTML =(data.daily[i].temp.day - 273.15).toFixed(0) + "°";
+              document.getElementById('7days_humidity'+i).innerHTML = data.daily[i].humidity+ "%";
+              document.getElementById('7days_rain'+i).innerHTML = data.daily[i].rain;
+              document.getElementById('7days_wSpeed'+i).innerHTML = data.daily[i].wind_speed + "meter/sec";
+              document.getElementById('7days_clouds'+i).innerHTML = data.daily[i].clouds+ "%";
+             }
           })
       })       
         .catch((err) => {
@@ -49,4 +65,3 @@ function showPosition(position) {
     }); 
         
   }
-
